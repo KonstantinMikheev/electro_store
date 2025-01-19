d31d3bd5f08d8bf12ed79edd6c59f54b7d06f9c2
KonstantinMikheev <mikhey17@gmail.com>
2025-01-19 16:31:58 +0300

    Добавлены модели в приложение trading, добавлена авторизация JWT

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,15 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY = 'django-insecure-t!nhcos81$fc-c1y%!i2ploqun@aq4$d505733$_4b0gv)pp#f'
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', False) == True
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -20,6 +23,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
 
     'users',
+    'trading',
 ]
 
 MIDDLEWARE = [
@@ -57,8 +61,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'POSTGRES_HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
